41fc34b04d9f506e71734504ffa959d023b3f8ff
Ishaan Desai <ishaandesai@gmail.com>
2022-08-01 14:48:25 +0200

    Repair broken workflow of running macro-micro dummy

--- examples/macro-micro-dummy/run_micro_manager.py
+++ examples/macro-micro-dummy/run_micro_manager.py
@@ -4,6 +4,8 @@ Script to run the Micro Manager
 
 from micro_manager import MicroManager
 
-manager = MicroManager("micro-manager-config.json")
+manager = MicroManager("./micro-manager-config.json")
 
-manager.run()
+manager.initialize()
+
+manager.solve()
