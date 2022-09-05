698387d984675fbd8c51bfd434f41fdaaddd2476
Ishaan Desai <ishaandesai@gmail.com>
2022-09-05 15:55:45 +0200

    Bump version, formatting, and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## latest
 
+## v0.2.0rc1
+
+- Change package from `micro-manager` to `micro-manager-precice`.
+
 ## v0.1.0
 
 - First release of Micro Manager prototype. Important features: Micro Manager can run in parallel, capability to handle bi-directional implicit coupling
--- setup.py
+++ setup.py
@@ -14,17 +14,21 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='micro-manager-precice',
-    version='v0.1.0',
+    version='v0.2.0rc1',
     description='micro-manager-precice is a package which facilitates two-scale macro-micro coupled simulations using preCICE',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/precice/micro-manager',
-    entry_points={'console_scripts': [
-        'micro_manager=micro_manager.micro_manager:main']},
+    entry_points={
+        'console_scripts': ['micro_manager=micro_manager.micro_manager:main']},
     author='Ishaan Desai',
     author_email='ishaan.desai@uni-stuttgart.de',
     license='LGPL-3.0',
-    packages=find_packages(exclude=['examples']),
-    install_requires=['pyprecice>=2.3.0', 'numpy>=1.13.3', 'mpi4py'],
+    packages=find_packages(
+        exclude=['examples']),
+    install_requires=[
+        'pyprecice>=2.3.0',
+        'numpy>=1.13.3',
+        'mpi4py'],
     test_suite='tests',
     zip_safe=False)
