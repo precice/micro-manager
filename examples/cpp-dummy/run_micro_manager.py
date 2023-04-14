8968f01316f7fe75d27e6331688fc783ce1a7e04
Erik Scheurer <erik.scheurer@gmx.de>
2023-04-14 15:17:05 +0200

    Correct path and make config file mandatory

--- examples/cpp-dummy/run_micro_manager.py
+++ examples/cpp-dummy/run_micro_manager.py
@@ -6,7 +6,7 @@ from micro_manager import MicroManager
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("--config", help="Path to the micro manager configuration file")
+parser.add_argument("--config", required=True, help="Path to the micro manager configuration file")
 args = parser.parse_args()
 
 manager = MicroManager(args.config)
