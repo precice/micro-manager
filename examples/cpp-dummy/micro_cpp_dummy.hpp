72956b440dc2f6c4e349993f7e19036c13627e31
Ishaan Desai <ishaandesai@gmail.com>
2023-04-15 19:05:37 +0200

    Use py::tuple instead of std::tuple in cpp solverdummy

--- examples/cpp-dummy/micro_cpp_dummy.hpp
+++ examples/cpp-dummy/micro_cpp_dummy.hpp
@@ -22,7 +22,7 @@ public:
     void reload_checkpoint();
 
     void setState(double micro_scalar_data, double checkpoint);
-    std::tuple<double, double, double> getState() const;
+    py::tuple<double, double, double> getState() const;
 
 private:
     int _sim_id;
