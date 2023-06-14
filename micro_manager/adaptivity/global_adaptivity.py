419c62ec3dbca622792f09ee8f660ef78b8d2782
Ishaan Desai <ishaandesai@gmail.com>
2023-06-14 10:12:20 +0200

    Add comments on send map and receive map dictionaries

--- micro_manager/adaptivity/global_adaptivity.py
+++ micro_manager/adaptivity/global_adaptivity.py
@@ -20,8 +20,8 @@ class GlobalAdaptivityCalculator(AdaptivityCalculator):
         self._global_ids = global_ids
         self._comm = comm
         self._rank = rank
-        self._send_sims_from_this_rank = dict()
-        self._recv_sims_from_ranks = dict()  # keys are global IDs, values are ranks to receive from
+        self._send_sims_from_this_rank = dict()  # keys are global IDs of sims to send, values are ranks to send the sims to
+        self._recv_sims_from_ranks = dict()  # keys are global IDs to receive, values are ranks to receive from
 
     def update_inactive_sims(
             self,
@@ -34,12 +34,11 @@ class GlobalAdaptivityCalculator(AdaptivityCalculator):
 
         _micro_sim_states = np.copy(micro_sim_states)  # Input micro_sim_states is not longer used after this point
         number_of_sims = _micro_sim_states.size
-
-        # keys are associated global active IDs, values are global inactive IDs which are to be activated, and
-        inactive_to_associated_active_map = dict()
+       
+        inactive_to_associated_active_map = dict()  # keys are associated global active IDs, values are global inactive IDs which are to be activated
         send_sims_to_ranks_local = dict()  # keys are global IDs, values are rank to send to
 
-        # Update the set of inactive micro sims
+        # Fill dicts send_sims_from_this_rank and recv_sims_from_ranks. Names are self explanatory.
         for global_id in range(number_of_sims):
             if not _micro_sim_states[global_id]:  # if id is inactive
                 if self._check_for_activation(global_id, similarity_dists, _micro_sim_states):
