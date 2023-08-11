675cfdbe52ff428fb192aa6f86d406638b43b514
Ishaan Desai <ishaandesai@gmail.com>
2023-08-11 14:42:16 -0400

    Port unit_cube.py to v3

--- tests/integration/test_unit_cube/unit_cube.py
+++ tests/integration/test_unit_cube/unit_cube.py
@@ -15,14 +15,9 @@ def main():
     t_end = 10
 
     # preCICE setup
-    interface = precice.Interface("macro-cube", "precice-config.xml", 0, 1)
-
-    # define coupling meshes
-    read_mesh_name = write_mesh_name = "macro-cube-mesh"
-    read_mesh_id = interface.get_mesh_id(read_mesh_name)
+    participant = precice.Participant("macro-cube", "precice-config.xml", 0, 1)
+    mesh_name = "macro-cube-mesh"
     read_data_names = {"micro-scalar-data": 0, "micro-vector-data": 1}
-
-    write_mesh_id = interface.get_mesh_id(write_mesh_name)
     write_data_names = {"macro-scalar-data": 0, "macro-vector-data": 1}
 
     # Coupling mesh - unit cube with 5 points in each direction
@@ -33,11 +28,8 @@ def main():
         np.linspace(0, 1, np_axis)
     )
 
-    nv = np_axis ** interface.get_dimensions()
-    coords = np.zeros((nv, interface.get_dimensions()))
-
-    write_scalar_data = np.zeros(nv)
-    write_vector_data = np.zeros((nv, interface.get_dimensions()))
+    nv = np_axis ** participant.get_mesh_dimensions(mesh_name)
+    coords = np.zeros((nv, participant.get_mesh_dimensions(mesh_name)))
 
     # Define unit cube coordinates
     for z in range(np_axis):
@@ -48,6 +40,13 @@ def main():
                 coords[n, 1] = y_coords[x, y, z]
                 coords[n, 2] = z_coords[x, y, z]
 
+    # Define points on entire domain as coupling mesh
+    vertex_ids = participant.set_mesh_vertices(mesh_name, coords)
+
+    write_data = []
+    write_data.append(np.zeros(nv))
+    write_data.append(np.zeros((nv, participant.get_mesh_dimensions(mesh_name))))
+
     # Define initial data to write to preCICE
     scalar_value = 1.0
     vector_value = [2.0, 3.0, 4.0]
@@ -55,58 +54,38 @@ def main():
         for y in range(np_axis):
             for x in range(np_axis):
                 n = x + y * np_axis + z * np_axis * np_axis
-                write_scalar_data[n] = scalar_value
-                write_vector_data[n, 0] = vector_value[0]
-                write_vector_data[n, 1] = vector_value[1]
-                write_vector_data[n, 2] = vector_value[2]
+                write_data[0][n] = scalar_value
+                write_data[1][n, 0] = vector_value[0]
+                write_data[1][n, 1] = vector_value[1]
+                write_data[1][n, 2] = vector_value[2]
         scalar_value += 1
         vector_value = [x + 1 for x in vector_value]
 
-    # Define Gauss points on entire domain as coupling mesh
-    vertex_ids = interface.set_mesh_vertices(read_mesh_id, coords)
+    # Write initial data to preCICE
+    if participant.requires_initial_data():
+        for count, data_name in enumerate(write_data_names.keys()):
+            participant.write_data(mesh_name, data_name, vertex_ids, write_data[count])
 
-    read_data_ids = dict()
-    # coupling data
-    for name, dim in read_data_names.items():
-        read_data_ids[name] = interface.get_data_id(name, read_mesh_id)
+    participant.initialize()
 
-    write_data_ids = dict()
-    for name, dim in write_data_names.items():
-        write_data_ids[name] = interface.get_data_id(name, write_mesh_id)
-
-    # initialize preCICE
-    dt = interface.initialize()
-
-    # Set initial data to write to preCICE
-    if interface.is_action_required(precice.action_write_initial_data()):
-        for name, dim in write_data_names.items():
-            if dim == 0:
-                interface.write_block_scalar_data(write_data_ids[name], vertex_ids, write_scalar_data)
-            elif dim == 1:
-                interface.write_block_vector_data(write_data_ids[name], vertex_ids, write_vector_data)
-        interface.mark_action_fulfilled(precice.action_write_initial_data())
-
-    interface.initialize_data()
+    read_data = []
+    dt = participant.get_max_time_step_size()
 
     # time loop
-    while interface.is_coupling_ongoing():
+    while participant.is_coupling_ongoing():
         # write checkpoint
-        if interface.is_action_required(precice.action_write_iteration_checkpoint()):
+        if participant.requires_writing_checkpoint():
             print("Saving macro state")
             t_checkpoint = t
             n_checkpoint = n
-            interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
 
         # Read data from preCICE
-        for name, dim in read_data_names.items():
-            if dim == 0:
-                read_scalar_data = interface.read_block_scalar_data(read_data_ids[name], vertex_ids)
-            elif dim == 1:
-                read_vector_data = interface.read_block_vector_data(read_data_ids[name], vertex_ids)
+        for count, data_name in enumerate(read_data_names.keys()):
+            read_data[count] = participant.read_data(mesh_name, data_name, vertex_ids, 1.)
 
         # Set the read data as the write data with an increment
-        write_scalar_data = read_scalar_data + 1
-        write_vector_data = read_vector_data + 1
+        write_data[0] = read_data[0] + 1
+        write_data[1] = read_data[1] + 1
 
         # Define new data to write to preCICE midway through the simulation
         if t == t_end / 2:
@@ -116,32 +95,28 @@ def main():
                 for y in range(np_axis):
                     for x in range(np_axis):
                         n = x + y * np_axis + z * np_axis * np_axis
-                        write_scalar_data[n] = scalar_value
-                        write_vector_data[n, 0] = vector_value[0]
-                        write_vector_data[n, 1] = vector_value[1]
-                        write_vector_data[n, 2] = vector_value[2]
+                        write_data[0][n] = scalar_value
+                        write_data[1][n, 0] = vector_value[0]
+                        write_data[1][n, 1] = vector_value[1]
+                        write_data[1][n, 2] = vector_value[2]
 
         # Write data to preCICE
-        for name, dim in write_data_names.items():
-            if dim == 0:
-                interface.write_block_scalar_data(write_data_ids[name], vertex_ids, write_scalar_data)
-            elif dim == 1:
-                interface.write_block_vector_data(write_data_ids[name], vertex_ids, write_vector_data)
+        for count, data_name in enumerate(write_data_names.keys()):
+            participant.write_data(mesh_name, data_name, vertex_ids, write_data[count])
 
-        # do the coupling
-        dt = interface.advance(dt)
+        participant.advance(dt)
+        dt = participant.get_max_time_step_size()
 
         # advance variables
         n += 1
         t += dt
 
-        if interface.is_action_required(precice.action_read_iteration_checkpoint()):
+        if participant.requires_reading_checkpoint():
             print("Reverting to old macro state")
             t = t_checkpoint
             n = n_checkpoint
-            interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
 
-    interface.finalize()
+    participant.finalize()
 
 
 if __name__ == '__main__':
