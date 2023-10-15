e98703915ca6214b425d2789b0e2a1406a7b4671
Ishaan Desai <ishaandesai@gmail.com>
2023-10-14 23:00:00 -0400

    Remove function set_global_id from Simulation class, as we should never have to change the global ID of a micro simulation

--- micro_manager/micro_simulation.py
+++ micro_manager/micro_simulation.py
@@ -27,10 +27,4 @@ def create_simulation_class(micro_simulation_class):
         def get_global_id(self) -> int:
             return self._global_id
 
-        def set_global_id(self, global_id) -> None:
-            # TODO: Simulation objects have been initialized with a global ID, which
-            # we can no longer change, so change global ID here is problematic and
-            # will create a mismatch.
-            self._global_id = global_id
-
     return Simulation
