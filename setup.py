736b552e3730bc15a6b083b5120f00033489c6a3
Ishaan Desai <ishaandesai@gmail.com>
2024-02-09 10:52:48 +0100

    Change pyprecice dependency version to v3.0.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(
         exclude=['examples']),
     install_requires=[
-        'pyprecice>=3.0.0.0dev2',
+        'pyprecice>=3.0.0.0',
         'numpy>=1.13.3',
         'mpi4py'],
     test_suite='tests',
