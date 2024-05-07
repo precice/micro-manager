912a946f60171f4081539303638af62aafd3bb32
Torben Schiz <schitjws@gmail.com>
2024-05-07 16:26:33 +0200

    Add snapshot test, improve snapshot and restructure

--- tests/unit/test_hdf5_functionality.py
+++ tests/unit/test_hdf5_functionality.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import h5py
 
-from micro_manager.read_write_data import HDFParameterfile
+from snapshot.read_write_hdf import ReadWriteHDF
 
 
 class TestHDFFunctionalities(TestCase):
@@ -13,88 +13,126 @@ class TestHDFFunctionalities(TestCase):
         """
         Test if file creation works as expected
         """
-        dir_name = os.path.dirname(os.path.realpath(__file__))
-        file_name = dir_name + "/database_0.hdf5"
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "hdf_files")
+        file_name = "create_file.hdf5"
+        entire_path = os.path.join(path, file_name)
+        if os.path.isfile(entire_path):
+            os.remove(entire_path)
+        data_manager = ReadWriteHDF(MagicMock())
+        complete_path = data_manager.create_file(path, file_name)
+        self.assertTrue(os.path.isfile(complete_path))
+        os.remove(complete_path)
+
+    def test_collect_output_files(self):
+        """
+        Test if collection of output files form different ranks works as expected
+        """
+        dir_name = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "hdf_files"
+        )
+        files = ["output_1.hdf5", "output_2.hdf5"]
+        if os.path.isfile(os.path.join(dir_name, "output.hdf5")):
+            os.remove(os.path.join(dir_name, "output.hdf5"))
+        output1 = h5py.File(os.path.join(dir_name, "output_1.hdf5"), "r")
+        output2 = h5py.File(os.path.join(dir_name, "output_2.hdf5"), "r")
+        data_manager = ReadWriteHDF(MagicMock())
+        data_manager.collect_output_files(dir_name, files)
+        output = h5py.File(os.path.join(dir_name, "output.hdf5"), "r")
+        self.assertEqual(
+            output["macro_scalar_data"][0], output1["macro_scalar_data"][0]
+        )
+        self.assertEqual(
+            output["macro_scalar_data"][1], output2["macro_scalar_data"][0]
+        )
+        self.assertListEqual(
+            output["macro_vector_data"][0].tolist(),
+            output1["macro_vector_data"][0].tolist(),
+        )
+        self.assertListEqual(
+            output["macro_vector_data"][1].tolist(),
+            output2["macro_vector_data"][0].tolist(),
+        )
+        self.assertEqual(
+            output["micro_scalar_data"][0], output1["micro_scalar_data"][0]
+        )
+        self.assertEqual(
+            output["micro_scalar_data"][1], output2["micro_scalar_data"][0]
+        )
+        self.assertListEqual(
+            output["micro_vector_data"][0].tolist(),
+            output1["micro_vector_data"][0].tolist(),
+        )
+        self.assertListEqual(
+            output["micro_vector_data"][1].tolist(),
+            output2["micro_vector_data"][0].tolist(),
+        )
+        output1.close()
+        output2.close()
+        output.close()
+        os.remove(os.path.join(dir_name, "output.hdf5"))
+
+    def test_simulation_output_to_hdf(self):
+        """
+        Test if the write_sim_dict_to_hdf method correctly writes a dictionary or list of dictionaries to an hdf5 file.
+        """
+        file_name = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "hdf_files",
+            "write_output.hdf5",
+        )
         if os.path.isfile(file_name):
             os.remove(file_name)
-        data_manager = HDFParameterfile(MagicMock(), 0, 1)
-        data_manager.create_file(dir_name)
-        self.assertTrue(os.path.isfile(file_name))
-        os.remove(file_name)
 
-    def test_collect_parameter_files(self):
-        pass
+        data_manager = ReadWriteHDF(MagicMock())
+        macro_data = {
+            "macro_vector_data": np.array([3, 1, 2]),
+            "macro_scalar_data": 2,
+        }
+        micro_data = {
+            "micro_vector_data": np.array([3, 2, 1]),
+            "micro_scalar_data": 1,
+        }
 
-    def test_dict_to_hdf(self):
-        """
-        Test if the write_dict_to_hdf method correctly writes a dictionary or list of dictionaries to an hdf5 file.
-        """
-        file_name = (
-            os.path.dirname(os.path.realpath(__file__)) + "/test_dict_to_hdf.hdf5"
-        )
+        expected_micro_vector_data = np.array([3, 2, 1])
+        expected_micro_scalar_data = 1
 
-        data_manager = HDFParameterfile(MagicMock(), 0, 1)
-        input_data = [
-            {
-                "macro_vector_data": np.array([0, 1, 2]),
-                "macro_scalar_data": 1,
-                "micro_vector_data": np.array([3, 1, 2]),
-                "micro_scalar_data": 2,
-            }
-        ] * 2
-        expected_macro_vector_data = [np.array([0, 1, 2]), np.array([0, 1, 2])]
-        expected_macro_scalar_data = [1, 1]
-        expected_micro_vector_data = [np.array([3, 1, 2]), np.array([3, 1, 2])]
-        expected_micro_scalar_data = [2, 2]
+        expected_macro_vector_data = np.array([3, 1, 2])
+        expected_macro_scalar_data = 2
 
-        data_manager.write_dict_to_hdf(file_name, input_data, "w")
+        data_manager.write_sim_output_to_hdf(file_name, macro_data, micro_data)
 
         test_file = h5py.File(file_name, "r")
 
-        for i in range(2):
-            self.assertEqual(
-                (test_file["macro_scalar_data"][i]), (expected_macro_scalar_data[i])
-            )
-            self.assertListEqual(
-                (test_file["macro_vector_data"][i]).tolist(),
-                (expected_macro_vector_data[i]).tolist(),
-            )
-            self.assertEqual(
-                (test_file["micro_scalar_data"][i]), (expected_micro_scalar_data[i])
-            )
-            self.assertListEqual(
-                (test_file["micro_vector_data"][i]).tolist(),
-                (expected_micro_vector_data[i]).tolist(),
-            )
+        self.assertEqual(
+            (test_file["micro_scalar_data"][0]), expected_micro_scalar_data
+        )
+        self.assertListEqual(
+            (test_file["micro_vector_data"][0]).tolist(),
+            (expected_micro_vector_data).tolist(),
+        )
+        self.assertEqual(
+            (test_file["macro_scalar_data"][0]), expected_macro_scalar_data
+        )
+        self.assertListEqual(
+            (test_file["macro_vector_data"][0]).tolist(),
+            (expected_macro_vector_data).tolist(),
+        )
+        os.remove(file_name)
 
-    def test_sim_output_to_hdf(self):
-        pass
-
-    def test_hdf_to_parameter_dict(self):
+    def test_hdf_to_dict(self):
         """
         Test if read_parameter_hdf_to_dict method correctly reads parameter data from an hdf5 file.
         """
         expected_macro_scalar = 1
-        expected_macro_vector = np.array([0, 1, 2])
-        file_name = os.path.dirname(os.path.realpath(__file__)) + "/read_test.hdf5"
+        expected_macro_vector = np.array([1, 2, 3])
+        file_name = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "hdf_files", "output_1.hdf5"
+        )
         read_data_names = {"macro_vector_data": True, "macro_scalar_data": False}
-        data_manager = HDFParameterfile(MagicMock(), 0, 1)
-        [
-            {"macro_vector_data": np.array([0, 1, 2]), "macro_scalar_data": 1},
-            {"macro_vector_data": np.array([0, 1, 2]), "macro_scalar_data": 1},
-        ]
-        read = data_manager.read_parameter_hdf_to_dict(file_name, read_data_names)
-        for i in range(2):
+        data_manager = ReadWriteHDF(MagicMock())
+        read = data_manager.read_hdf_to_dict(file_name, read_data_names)
+        for i in range(len(read)):
             self.assertEqual(read[i]["macro_scalar_data"], expected_macro_scalar)
             self.assertListEqual(
                 read[i]["macro_vector_data"].tolist(), expected_macro_vector.tolist()
             )
-
-    def test_micro_hdf_to_dict(self):
-        pass
-
-    def test_hdf_to_sim_output(self):
-        pass
-
-    def test_all_hdf_to_dict(self):
-        pass
--- tests/unit/test_snapshot_computation.py
+++ tests/unit/test_snapshot_computation.py
@@ -1,7 +1,9 @@
 import numpy as np
+import os
 from unittest import TestCase
-from unittest.mock import MagicMock
-import micro_manager
+from unittest.mock import MagicMock, patch
+import snapshot
+from micro_manager.micro_simulation import create_simulation_class
 
 
 class MicroSimulation:
@@ -20,15 +22,17 @@ class MicroSimulation:
         }
 
 
-class TestFunctioncalls(TestCase):
+class TestFunctionCalls(TestCase):
     def setUp(self):
         self.fake_read_data_names = {
             "macro-scalar-data": False,
             "macro-vector-data": True,
         }
-        self.fake_read_data = [
-            {"macro-scalar-data": 1, "macro-vector-data": np.array([0, 1, 2])}
-        ] * 4
+        self.fake_read_data = {
+            "macro-scalar-data": 1,
+            "macro-vector-data": np.array([0, 1, 2]),
+        }
+
         self.fake_write_data_names = {
             "micro-scalar-data": False,
             "micro-vector-data": True,
@@ -42,16 +46,102 @@ class TestFunctioncalls(TestCase):
             }
         ] * 4
 
+    def test_snapshot_constructor(self):
+        """
+        Test if the constructor of the SnapshotComputation class passes correct values to member variables.
+        """
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
+        file_name = "output.hdf5"
+        complete_path = os.path.join(path, file_name)
+        if os.path.isfile(complete_path):
+            os.remove(complete_path)
+
+        snapshot_object = snapshot.SnapshotComputation("snapshot-config.json")
+
+        self.assertDictEqual(
+            snapshot_object._read_data_names, self.fake_read_data_names
+        )
+        self.assertDictEqual(
+            snapshot_object._write_data_names, self.fake_write_data_names
+        )
+        self.assertTrue(snapshot_object._postprocessing)
+        self.assertTrue(snapshot_object._merge_output)
+
+        self.assertEqual(snapshot_object._global_number_of_sims, 1)
+        self.assertDictEqual(
+            snapshot_object._read_data_names, self.fake_read_data_names
+        )
+        self.assertDictEqual(
+            snapshot_object._write_data_names, self.fake_write_data_names
+        )
+        self.assertTrue(os.path.isfile(complete_path))
+        os.remove(complete_path)
+        os.rmdir(path)
+
+    @patch("snapshot.SnapshotComputation._initialize")
+    def test_solve_micro_sims(self, mock_initialize):
+        """
+        Test if the internal function _solve_micro_simulations works as expected.
+        """
+        snapshot_object = snapshot.SnapshotComputation("snapshot-config.json")
+
+        snapshot_object._micro_problem = MicroSimulation
+
+        snapshot_object._micro_sims = create_simulation_class(
+            snapshot_object._micro_problem
+        )(0)
+
+        micro_sim_output = snapshot_object._solve_micro_simulations(self.fake_read_data)
+        self.assertEqual(micro_sim_output["micro-scalar-data"], 2)
+        self.assertListEqual(
+            micro_sim_output["micro-vector-data"].tolist(),
+            (self.fake_read_data["macro-vector-data"] + 1).tolist(),
+        )
+
+    @patch("snapshot.SnapshotComputation._initialize")
+    def test_solve(self, mock_initialize):
+        """
+        Test if the solve function of the SnapshotComputation class works as expected.
+        """
+
+        snapshot_object = snapshot.SnapshotComputation("snapshot-config.json")
+        snapshot_object._data_storage = MagicMock()
+        snapshot_object._file_name = "output.hdf5"
+        snapshot_object._output_file_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "output",
+            snapshot_object._file_name,
+        )
+
+        snapshot_object._output_subdirectory = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "output"
+        )
+        snapshot_object._macro_parameters = [self.fake_read_data]
+        snapshot_object._local_number_of_sims = 1
+        snapshot_object._global_number_of_sims = 1
+        snapshot_object._global_ids_of_local_sims = [0]
+        snapshot_object._micro_problem = MicroSimulation
+
+        snapshot_object.solve()
+
+        self.assertEqual(
+            snapshot_object._micro_sims.very_important_value, 0
+        )  # test inheritance
+
     def test_config(self):
         """
         Test if the functions in the SnapshotConfig class work.
         """
-        config = micro_manager.SnapshotConfig(MagicMock(), "snapshot-config.json")
+        config = snapshot.SnapshotConfig(MagicMock(), "snapshot-config.json")
 
-        self.assertEqual(config._parameter_file_name.split("/")[-1], "database.hdf5")
+        self.assertEqual(
+            config._parameter_file_name.split("/")[-1], "test_parameter.hdf5"
+        )
         self.assertEqual(config._micro_file_name, "test_snapshot_computation")
         self.assertDictEqual(config._read_data_names, self.fake_read_data_names)
-        self.assertDictEqual(self.fake_write_data_names, config._write_data_names)
+        self.assertDictEqual(config._write_data_names, self.fake_write_data_names)
+        self.assertTrue(config._postprocessing)
+        self.assertTrue(config._merge_output)
 
 
 if __name__ == "__main__":
