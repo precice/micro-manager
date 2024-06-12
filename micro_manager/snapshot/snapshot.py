c2ee68f20f8904158bd48a1da6f092c97780f176
Torben Schiz <schitjws@gmail.com>
2024-06-12 15:17:27 +0200

    Restructure snapshot computation

--- micro_manager/snapshot/snapshot.py
+++ micro_manager/snapshot/snapshot.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """
-Snapshot Computation is a tool to initialize micro simulations and create a snapshot database by simulating
-micro simulations related to a set of prescribed macro parameters.
+Snapshot Computation is a tool withing the Micro Manager to initialize micro simulations and create a snapshot database by simulating micro simulations related to a set of prescribed macro parameters.
 This files the class SnapshotComputation which has the following callable public methods:
 
 - solve
+- initialize
 
 This file is directly executable as it consists of a main() function. Upon execution, an object of the class SnapshotComputation is created using a given JSON file,
- the solve method is called.
+the solve method is called.
 
 Detailed documentation: https://precice.org/tooling-micro-manager-overview.html
 """
 
-import argparse
 import importlib
 import os
 import sys
@@ -48,13 +47,15 @@ class MicroManagerSnapshot(MicroManager):
         # Path to the parameter file containing input parameters for micro simulations
         self._parameter_file = self._config.get_parameter_file_name()
 
+        # Get name of pos-processing script
         self._post_processing_file_name = self._config.get_postprocessing_file_name()
-
-        self._merge_output_files = self._config.get_merge_output()
 
         self._is_micro_solve_time_required = self._config.write_micro_solve_time()
 
-        self._dt = self._config.get_time_step_size()
+        self._micro_dt = self._config.get_time_step_size()
+
+        # Collect crashed indices
+        self._crashed_snapshots = []  # Declaration
 
     # **************
     # Public methods
@@ -63,8 +64,10 @@ class MicroManagerSnapshot(MicroManager):
     def solve(self) -> None:
         """
         Solve the problem by iterating over a set macro parameters.
-        - Read macro parameters from the config file, solve micro simulation one by one,
-          and write outputs to a file.
+        - Create micro simulation object.
+        - Post-process micro output.
+        - Write output to database.
+        - Merge output in parallel run.
         """
 
         # Loop over all macro parameters
@@ -80,8 +83,9 @@ class MicroManagerSnapshot(MicroManager):
 
             # Write output to file
             if micro_sims_output is not None:
-                # Postprocessing
+                # Post-processing
                 if self._post_processing_file_name is not None:
+                    # Attempt importing post-processing script
                     try:
                         post_processing = getattr(
                             importlib.import_module(
@@ -106,9 +110,13 @@ class MicroManagerSnapshot(MicroManager):
                         )
                         self._post_processing_file_name = None
                 self._data_storage.write_output_to_hdf(
-                    self._output_file_path, micro_sims_input, micro_sims_output
+                    self._output_file_path,
+                    micro_sims_input,
+                    micro_sims_output,
+                    elems,
+                    len(self._macro_parameters),
                 )
-            # Log error and skip snapshot
+            # Log error and wrote macro data to database if simulation has crashed
             else:
                 parameter = ""
                 for key, value in micro_sims_input.items():
@@ -119,19 +127,37 @@ class MicroManagerSnapshot(MicroManager):
                         parameter
                     )
                 )
+                self._data_storage.write_output_to_hdf(
+                    self._output_file_path,
+                    micro_sims_input,
+                    None,
+                    elems,
+                    len(self._macro_parameters),
+                )
 
-        # If merging of output files is activated in config, merge output files
-        if self._merge_output_files and self._is_parallel:
+                self._crashed_snapshots.append(elems)
+
+        # Write positions of crashed snapshots in database to database
+        if len(self._crashed_snapshots) > 0:
+            self._data_storage.write_crashed_snapshots_macro(
+                self._output_file_path, self._crashed_snapshots
+            )
+        self._data_storage.set_status(self._output_file_path, "none")
+
+        # Merge output files
+        if self._is_parallel:
             self._logger.info(
                 "Snapshots have been computed and stored. Merging output files"
             )
+            self._data_storage.set_status(self._output_file_path, "reading/deleting")
             list_of_output_files = self._comm.gather(self._file_name, 0)
             if self._rank == 0:
                 self._data_storage.collect_output_files(
-                    self._output_subdirectory, list_of_output_files
+                    self._output_subdirectory,
+                    list_of_output_files,
+                    self._parameter_space_size,
                 )
-        else:
-            self._logger.info("Snapshot computation completed.")
+        self._logger.info("Snapshot computation completed.")
 
     def initialize(self) -> None:
         """
@@ -154,7 +180,7 @@ class MicroManagerSnapshot(MicroManager):
         self._macro_parameters = self._data_storage.read_hdf(
             self._parameter_file, self._read_data_names
         )
-
+        self._parameter_space_size = len(self._macro_parameters)
         # Decompose parameters if the snapshot creation is executed in parallel
         if self._is_parallel:
             equal_partition = int(len(self._macro_parameters) / self._size)
@@ -167,11 +193,11 @@ class MicroManagerSnapshot(MicroManager):
                 end = start + equal_partition
             self._macro_parameters = self._macro_parameters[start:end]
 
-        # Create file to store output from a rank in
+        # Create database file to store output from a rank in
         if self._is_parallel:
-            self._file_name = "output_{}.hdf5".format(self._rank)
+            self._file_name = "snapshot_data_{}.hdf5".format(self._rank)
         else:
-            self._file_name = "output.hdf5"
+            self._file_name = "snapshot_data.hdf5"
         self._output_file_path = os.path.join(
             self._output_subdirectory, self._file_name
         )
@@ -208,6 +234,7 @@ class MicroManagerSnapshot(MicroManager):
         for i in range(self._local_number_of_sims):
             self._global_ids_of_local_sims.append(sim_id)
             sim_id += 1
+
         self._micro_problem = getattr(
             importlib.import_module(
                 self._config.get_micro_file_name(), "MicroSimulation"
@@ -227,12 +254,12 @@ class MicroManagerSnapshot(MicroManager):
 
     def _solve_micro_simulation(self, micro_sims_input: dict) -> dict | None:
         """
-        Solve micro simulation.
+        Solve a single micro simulation.
 
         Parameters
         ----------
         micro_sims_input : dict
-            Dict in which keys are names of data and the values are the data which are required inputs to
+            Keys are names of data and the values are the data which are required inputs to
             solve a micro simulation.
 
         Returns
@@ -243,7 +270,7 @@ class MicroManagerSnapshot(MicroManager):
         """
         try:
             start_time = time.time()
-            micro_sims_output = self._micro_sims.solve(micro_sims_input, self._dt)
+            micro_sims_output = self._micro_sims.solve(micro_sims_input, self._micro_dt)
             end_time = time.time()
 
             if self._is_micro_solve_time_required:
