0c42d356eebd92714c18ab0c65a1ef89e13e01c1
Ishaan Desai <ishaandesai@gmail.com>
2025-04-01 15:54:36 +0200

    Remove last traces of the old names of logging wrapper functions

--- micro_manager/adaptivity/global_adaptivity.py
+++ micro_manager/adaptivity/global_adaptivity.py
@@ -190,7 +190,7 @@ class GlobalAdaptivityCalculator(AdaptivityCalculator):
         global_active_sims = np.count_nonzero(self._is_sim_active)
         global_inactive_sims = np.count_nonzero(self._is_sim_active == False)
 
-        self._metrics_logger.log_info_one_rank(
+        self._metrics_logger.log_info_rank_zero(
             "{},{},{},{},{},{}".format(
                 n,
                 np.mean(global_active_sims),
