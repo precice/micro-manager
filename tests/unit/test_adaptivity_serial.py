5882372795d708f787633eb59a382d44ccd48aec
Ishaan Desai <ishaandesai@gmail.com>
2025-07-30 11:20:59 +0200

    Fix constructor args to adaptivity base class in serial tests

--- tests/unit/test_adaptivity_serial.py
+++ tests/unit/test_adaptivity_serial.py
@@ -91,7 +91,7 @@ class TestLocalAdaptivity(TestCase):
         )
 
         adaptivity_controller = AdaptivityCalculator(
-            configurator, comm_world=MPI.COMM_WORLD, rank=0, nsims=self._number_of_sims
+            configurator, rank=0, nsims=self._number_of_sims
         )
         adaptivity_controller._hist_param = 0.5
         adaptivity_controller._adaptivity_data_names = [
@@ -139,7 +139,7 @@ class TestLocalAdaptivity(TestCase):
         )
 
         adaptivity_controller = AdaptivityCalculator(
-            configurator, comm_world=MPI.COMM_WORLD, rank=0, nsims=self._number_of_sims
+            configurator, rank=0, nsims=self._number_of_sims
         )
         adaptivity_controller._refine_const = self._refine_const
         adaptivity_controller._coarse_const = self._coarse_const
@@ -171,7 +171,7 @@ class TestLocalAdaptivity(TestCase):
         )
 
         adaptivity_controller = AdaptivityCalculator(
-            configurator, comm_world=MPI.COMM_WORLD, rank=0, nsims=self._number_of_sims
+            configurator, rank=0, nsims=self._number_of_sims
         )
 
         fake_data = np.array([[1], [2], [3]])
@@ -265,7 +265,7 @@ class TestLocalAdaptivity(TestCase):
         )
 
         adaptivity_controller = AdaptivityCalculator(
-            configurator, comm_world=MPI.COMM_WORLD, rank=0, nsims=self._number_of_sims
+            configurator, rank=0, nsims=self._number_of_sims
         )
         adaptivity_controller._refine_const = self._refine_const
         adaptivity_controller._coarse_const = self._coarse_const
