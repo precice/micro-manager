8f302bff8b046af827e85b67e43d50c6e9878ec8
Alex Hocks <alex.hocks@tum.de>
2026-02-25 13:43:37 +0100

    ?

--- tests/unit/test_tasking.py
+++ tests/unit/test_tasking.py
@@ -6,7 +6,7 @@ import sys
 from pathlib import Path
 # no clue why this TestCase does not see the local precice.py
 # So we add path here and load it
-sys.path.append(os.getcwd())
+sys.path.append(str(Path(__file__).resolve().parent))
 import precice
 
 import numpy as np
